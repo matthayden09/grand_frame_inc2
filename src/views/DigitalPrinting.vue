9498070624f10c337feee00e0a2c57acf3c7e89c
Matthew Hayden <matthayden09@gmail.com>
2022-02-23 23:34:04 -0600

    added page layouts

--- src/views/DigitalPrinting.vue
+++ src/views/DigitalPrinting.vue
@@ -1,230 +1,110 @@
 <template>
-  <!-- <div class="relative py-16 bg-white overflow-hidden">
-    <div class="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
-      <div
-        class="relative h-full text-lg max-w-prose mx-auto"
-        aria-hidden="true"
-      >
-        <svg
-          class="absolute top-12 left-full transform translate-x-32"
-          width="404"
-          height="384"
-          fill="none"
-          viewBox="0 0 404 384"
-        >
-          <defs>
-            <pattern
-              id="74b3fd99-0a6f-4271-bef2-e80eeafdf357"
-              x="0"
-              y="0"
-              width="20"
-              height="20"
-              patternUnits="userSpaceOnUse"
-            >
-              <rect
-                x="0"
-                y="0"
-                width="4"
-                height="4"
-                class="text-gray-200"
-                fill="currentColor"
-              />
-            </pattern>
-          </defs>
-          <rect
-            width="404"
-            height="384"
-            fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)"
-          />
-        </svg>
-        <svg
-          class="
-            absolute
-            top-1/2
-            right-full
-            transform
-            -translate-y-1/2 -translate-x-32
-          "
-          width="404"
-          height="384"
-          fill="none"
-          viewBox="0 0 404 384"
-        >
-          <defs>
-            <pattern
-              id="f210dbf6-a58d-4871-961e-36d5016a0f49"
-              x="0"
-              y="0"
-              width="20"
-              height="20"
-              patternUnits="userSpaceOnUse"
-            >
-              <rect
-                x="0"
-                y="0"
-                width="4"
-                height="4"
-                class="text-gray-200"
-                fill="currentColor"
-              />
-            </pattern>
-          </defs>
-          <rect
-            width="404"
-            height="384"
-            fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
-          />
-        </svg>
-        <svg
-          class="absolute bottom-12 left-full transform translate-x-32"
-          width="404"
-          height="384"
-          fill="none"
-          viewBox="0 0 404 384"
-        >
-          <defs>
-            <pattern
-              id="d3eb07ae-5182-43e6-857d-35c643af9034"
-              x="0"
-              y="0"
-              width="20"
-              height="20"
-              patternUnits="userSpaceOnUse"
-            >
-              <rect
-                x="0"
-                y="0"
-                width="4"
-                height="4"
-                class="text-gray-200"
-                fill="currentColor"
-              />
-            </pattern>
-          </defs>
-          <rect
-            width="404"
-            height="384"
-            fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)"
-          />
-        </svg>
-      </div>
-    </div>
-    <div class="relative px-4 sm:px-6 lg:px-8">
-      <div class="text-lg max-w-prose mx-auto">
-        <h1>
-          <span
-            class="
-              block
-              text-base text-center text-indigo-600
-              font-semibold
-              tracking-wide
-              uppercase
-            "
-            >Introducing</span
-          >
-          <span
+  <div class="bg-white">
+    <div class="max-w-7xl mx-auto py-24 sm:py-32 sm:px-2 lg:px-4">
+      <div class="max-w-2xl mx-auto px-4 lg:max-w-none">
+        <div class="max-w-3xl">
+          <h2 class="font-semibold text-gray-500">Drawstring Canister</h2>
+          <p
             class="
               mt-2
-              block
-              text-3xl text-center
-              leading-8
+              text-3xl
               font-extrabold
               tracking-tight
               text-gray-900
               sm:text-4xl
             "
-            >JavaScript for Beginners</span
           >
-        </h1>
-        <p class="mt-8 text-xl text-gray-500 leading-8">
-          Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At
-          arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae
-          feugiat egestas ac. Diam nulla orci at in viverra scelerisque eget.
-          Eleifend egestas fringilla sapien.
-        </p>
-      </div>
-      <div class="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
-        <p>
-          Faucibus commodo massa rhoncus, volutpat.
-          <strong>Dignissim</strong> sed <strong>eget risus enim</strong>.
-          Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent
-          donec est. Odio penatibus risus viverra tellus varius sit neque erat
-          velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget
-          risus enim. <a href="#">Mattis mauris semper</a> sed amet vitae sed
-          turpis id.
-        </p>
-        <ul role="list">
-          <li>Quis elit egestas venenatis mattis dignissim.</li>
-          <li>Cras cras lobortis vitae vivamus ultricies facilisis tempus.</li>
-          <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
-        </ul>
-        <p>
-          Quis semper vulputate aliquam venenatis egestas sagittis quisque orci.
-          Donec commodo sit viverra aliquam porttitor ultrices gravida eu.
-          Tincidunt leo, elementum mattis elementum ut nisl, justo, amet,
-          mattis. Nunc purus, diam commodo tincidunt turpis. Amet, duis sed elit
-          interdum dignissim.
-        </p>
-        <h2>From beginner to expert in 30 days</h2>
-        <p>
-          Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat
-          in. Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum
-          mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed
-          tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi.
-          Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis
-          diam.
-        </p>
-        <blockquote>
-          <p>
-            Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum
-            urna sed consectetur neque tristique pellentesque. Blandit amet, sed
-            aenean erat arcu morbi.
+            Use it your way
           </p>
-        </blockquote>
-        <p>
-          Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
-          enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
-          praesent donec est. Odio penatibus risus viverra tellus varius sit
-          neque erat velit.
-        </p>
-        <figure>
-          <img
-            class="w-full rounded-lg"
-            src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&auto=format&fit=facearea&w=1310&h=873&q=80&facepad=3"
-            alt=""
-            width="1310"
-            height="873"
-          />
-          <figcaption>
-            Sagittis scelerisque nulla cursus in enim consectetur quam.
-          </figcaption>
-        </figure>
-        <h2>Everything you need to get up and running</h2>
-        <p>
-          Purus morbi dignissim senectus mattis <a href="#">adipiscing</a>.
-          Amet, massa quam varius orci dapibus volutpat cras. In amet eu
-          ridiculus leo sodales cursus tristique. Tincidunt sed tempus ut
-          viverra ridiculus non molestie. Gravida quis fringilla amet eget dui
-          tempor dignissim. Facilisis auctor venenatis varius nunc, congue erat
-          ac. Cras fermentum convallis quam.
-        </p>
-        <p>
-          Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus
-          enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor
-          praesent donec est. Odio penatibus risus viverra tellus varius sit
-          neque erat velit.
-        </p>
+          <p class="mt-4 text-gray-500">
+            The Drawstring Canister comes with multiple strap and handle options
+            to adapt throughout your day. Shoulder sling it, backpack it, or
+            handy carry it.
+          </p>
+        </div>
+
+        <div
+          class="
+            space-y-16
+            pt-10
+            mt-10
+            border-t border-gray-200
+            sm:pt-16 sm:mt-16
+          "
+        >
+          <div
+            v-for="feature in features"
+            :key="feature.name"
+            class="
+              flex flex-col-reverse
+              lg:grid lg:grid-cols-12 lg:gap-x-8 lg:items-center
+            "
+          >
+            <div class="mt-6 lg:mt-0 lg:col-span-5 xl:col-span-4">
+              <h3 class="text-lg font-medium text-gray-900">
+                {{ feature.name }}
+              </h3>
+              <p class="mt-2 text-sm text-gray-500">
+                {{ feature.description }}
+              </p>
+            </div>
+            <div class="flex-auto lg:col-span-7 xl:col-span-8">
+              <div
+                class="
+                  rounded-lg
+                  bg-gray-100
+                  overflow-hidden
+                "
+              >
+                <img
+                  :src="feature.imageSrc"
+                  :alt="feature.imageAlt"
+                  class="object-center object-cover"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div> -->
+  </div>
   <Contact />
 </template>
 
 <script>
 import Contact from '../components/Contact.vue'
 
+const features = [
+  {
+    name: 'Adventure-ready',
+    description:
+      'The Drawstring Canister is water and tear resistant with durable canvas construction. This bag holds up to the demands of daily use while keeping your snacks secure.',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-feature-04-detail-03.jpg',
+    imageAlt: 'Printed photo of bag being tossed into the sky on top of grass.',
+  },
+  {
+    name: 'Minimal and clean',
+    description:
+      "Everything you need, nothing you don't. This bag has the simple, contemporary design that enables you to tell everyone you know about how essentialism is the only rational way to live life.",
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-feature-04-detail-01.jpg',
+    imageAlt: 'Double stitched black canvas hook loop.',
+  },
+  {
+    name: 'Organized',
+    description:
+      'Never lose your snacks again with our patent-pending snack stash pocket system. With dedicated pouches for each of your snacking needs, the Drawstring Canister unlocks new levels of efficiency and convenience.',
+    imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-feature-04-detail-02.jpg',
+    imageAlt: 'Black canvas body with chrome zipper and key ring.',
+  },
+]
+
 export default {
-  components: { Contact }
+  components: { Contact },
+  setup() {
+    return {
+      features,
+    }
+  },
 }
 </script>
 
